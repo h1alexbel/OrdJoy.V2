be930ba29cf929e36c6ba78812f8a73b5d4c79da
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-02-27 18:46:07 +0300

    update model package

--- database/src/main/java/com/ordjoy/database/model/review/AlbumReview.java
+++ database/src/main/java/com/ordjoy/database/model/review/AlbumReview.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -22,15 +24,28 @@ import javax.persistence.ManyToOne;
 @ToString
 @Entity
 @DiscriminatorValue("album_review")
-public class AlbumReview extends Review<Long> {
+public class AlbumReview extends Review {
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name =  "album_id")
+    @JoinColumn(name = "album_id")
     private Album album;
 
     @Builder
     public AlbumReview(Long id, String reviewText, User user, Album album) {
         super(id, reviewText, user);
         this.album = album;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AlbumReview that = (AlbumReview) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
