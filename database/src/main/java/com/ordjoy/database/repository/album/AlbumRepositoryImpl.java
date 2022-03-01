3d13fab0b43b89f231cc0d21d48c07ca68b102a0
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-01 19:51:54 +0300

    add album repository

---
+++ database/src/main/java/com/ordjoy/database/repository/album/AlbumRepositoryImpl.java
@@ -0,0 +1,66 @@
+package com.ordjoy.database.repository.album;
+
+import com.ordjoy.database.model.review.AlbumReview;
+import com.ordjoy.database.model.review.AlbumReview_;
+import com.ordjoy.database.model.track.Album;
+import com.ordjoy.database.model.track.Album_;
+import com.ordjoy.database.repository.AbstractGenericCRUDRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AlbumRepositoryImpl extends AbstractGenericCRUDRepository<Album, Long>
+        implements AlbumRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public AlbumRepositoryImpl(SessionFactory sessionFactory) {
+        super(Album.class, sessionFactory);
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Optional<Album> findAlbumByTitle(String title) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select a from Album a where a.title = :albumName", Album.class)
+                .setParameter("albumName", title)
+                .setMaxResults(1)
+                .getResultList()
+                .stream()
+                .findFirst();
+    }
+
+    @Override
+    public List<AlbumReview> findAlbumReviewsByAlbumTitle(String title) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<AlbumReview> criteria = cb.createQuery(AlbumReview.class);
+        Root<AlbumReview> root = criteria.from(AlbumReview.class);
+        Join<AlbumReview, Album> albumJoin = root.join(AlbumReview_.album);
+        criteria.select(root)
+                .where(cb.equal(albumJoin.get(Album_.title), title));
+        return session.createQuery(criteria).getResultList();
+    }
+
+    @Override
+    public List<AlbumReview> findAlbumReviewsByAlbumId(Long albumId) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<AlbumReview> criteria = cb.createQuery(AlbumReview.class);
+        Root<AlbumReview> root = criteria.from(AlbumReview.class);
+        Join<AlbumReview, Album> albumJoin = root.join(AlbumReview_.album);
+        criteria.select(root)
+                .where(cb.equal(albumJoin.get(Album_.id), albumJoin));
+        return session.createQuery(criteria).getResultList();
+    }
+}
