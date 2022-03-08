92d808953184c29989f7e614b10bd5eb08c29dea
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-08 20:18:04 +0300

    add review service interfaces

---
+++ service/src/main/java/com/ordjoy/service/service/review/AlbumReviewService.java
@@ -0,0 +1,7 @@
+package com.ordjoy.service.service.review;
+
+import com.ordjoy.database.model.review.AlbumReview;
+import com.ordjoy.service.dto.AlbumReviewDto;
+
+public interface AlbumReviewService extends ReviewService<AlbumReviewDto, AlbumReview, Long> {
+}
---
+++ service/src/main/java/com/ordjoy/service/service/review/TrackReviewService.java
@@ -0,0 +1,7 @@
+package com.ordjoy.service.service.review;
+
+import com.ordjoy.database.model.review.TrackReview;
+import com.ordjoy.service.dto.TrackReviewDto;
+
+public interface TrackReviewService extends ReviewService<TrackReviewDto, TrackReview, Long> {
+}
