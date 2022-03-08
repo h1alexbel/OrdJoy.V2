190b0ed246d236c5c37e244de8cce9f039ab6bb2
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-08 20:18:04 +0300

    add review service interfaces

---
+++ service/src/main/java/com/ordjoy/service/service/review/MixReviewService.java
@@ -0,0 +1,7 @@
+package com.ordjoy.service.service.review;
+
+import com.ordjoy.database.model.review.MixReview;
+import com.ordjoy.service.dto.MixReviewDto;
+
+public interface MixReviewService extends ReviewService<MixReviewDto, MixReview, Long> {
+}
