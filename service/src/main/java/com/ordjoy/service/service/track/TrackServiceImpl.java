255444443b1a46a01eb1db8b8bab2b13423a58cd
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-11 21:47:01 +0300

    update service layer

--- service/src/main/java/com/ordjoy/service/service/track/TrackServiceImpl.java
+++ service/src/main/java/com/ordjoy/service/service/track/TrackServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class TrackServiceImpl implements TrackService {
 
     private final TrackRepository trackRepository;
