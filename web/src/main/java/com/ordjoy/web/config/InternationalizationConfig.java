35814bda238b742abd2a9263eaf54ad521523a40
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-24 19:21:55 +0300

    add InternationalizationConfig

---
+++ web/src/main/java/com/ordjoy/web/config/InternationalizationConfig.java
@@ -0,0 +1,57 @@
+package com.ordjoy.web.config;
+
+import com.ordjoy.web.util.AttributeUtils;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@Configuration
+public class InternationalizationConfig implements WebMvcConfigurer {
+
+    private static final String BUNDLE_NAME = "translation";
+    private static final String STATIC_LOCATIONS = "/static/";
+    private static final String RESOURCE_DIRECTORY_HANDLER = "/resources/**";
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(BUNDLE_NAME);
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName(AttributeUtils.LANG_PARAM);
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(RESOURCE_DIRECTORY_HANDLER)
+                .addResourceLocations(STATIC_LOCATIONS);
+    }
+}
