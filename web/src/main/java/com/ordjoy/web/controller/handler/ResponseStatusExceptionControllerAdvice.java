1014000dedddc1df33a7e4113fcb9f7e485d1251
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-25 20:16:41 +0300

    add @Slf4j logs to the handlers

--- web/src/main/java/com/ordjoy/web/controller/handler/ResponseStatusExceptionControllerAdvice.java
+++ web/src/main/java/com/ordjoy/web/controller/handler/ResponseStatusExceptionControllerAdvice.java
@@ -1,17 +1,21 @@
 package com.ordjoy.web.controller.handler;
 
+import com.ordjoy.model.util.LoggingUtils;
 import com.ordjoy.web.util.AttributeUtils;
 import com.ordjoy.web.util.PageUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @ControllerAdvice
 public class ResponseStatusExceptionControllerAdvice {
 
     @ExceptionHandler(ResponseStatusException.class)
     public String handleResponseStatusException(Model model, ResponseStatusException e) {
+        log.debug(LoggingUtils.EXCEPTION_WAS_THROWN, e);
         model.addAttribute(AttributeUtils.EXCEPTION_MESSAGE, e.getMessage());
         return PageUtils.ERROR_PAGE;
     }
