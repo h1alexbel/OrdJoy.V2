0ab3cda10d94c3482facbabaad4193b5ca8b2812
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-04-14 16:18:19 +0300

    add utils for web module

---
+++ web/src/main/java/com/ordjoy/web/util/PageUtils.java
@@ -0,0 +1,58 @@
+package com.ordjoy.web.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PageUtils {
+
+    public static final String ALBUM_TRACKS_PAGE = "auth/albumTracks";
+    public static final String ADMIN_PAGE = "admin/adminMain";
+    public static final String METRICS_PAGE = "admin/metrics";
+    public static final String ERROR_PAGE = "error";
+    public static final String ALL_ALBUMS_PAGE = "auth/albums";
+    public static final String ALBUM_MAIN_PAGE = "auth/albumMain";
+    public static final String ADD_ALBUM_PAGE = "admin/addAlbumForm";
+    public static final String CONCRETE_ALBUM_REVIEWS_PAGE = "auth/concreteAlbumReviews";
+    public static final String ALBUM_REVIEWS_PAGE = "auth/albumReviews";
+    public static final String MIXES_PAGE = "auth/mixes";
+    public static final String ADD_MIX_FORM_PAGE = "admin/addMixForm";
+    public static final String MIX_MAIN_PAGE = "auth/mixMain";
+    public static final String MIX_REVIEWS_PAGE = "auth/mixReviews";
+    public static final String MIX_UPDATE_FORM = "admin/mixUpdateForm";
+    public static final String TRACKS_PAGE = "auth/tracks";
+    public static final String ADD_TRACK_FORM = "admin/addTrackForm";
+    public static final String ADD_TRACK_TO_MIX_FORM = "admin/addTrackToMixForm";
+    public static final String TRACK_MAIN_PAGE = "auth/trackMain";
+    public static final String TRACK_REVIEWS_PAGE = "auth/trackReviews";
+    public static final String TRACK_UPDATE_FORM = "admin/updateTrackForm";
+    public static final String ADD_ALBUM_REVIEW_FORM = "user/addAlbumReviewForm";
+    public static final String ADD_MIX_REVIEW_FORM = "user/addMixReviewForm";
+    public static final String ADD_TRACK_REVIEW_FORM = "user/addTrackReviewForm";
+    public static final String ALBUM_REVIEW_MAIN = "auth/albumReviewMain";
+    public static final String MIX_REVIEW_MAIN = "auth/mixReviewMain";
+    public static final String TRACK_REVIEW_MAIN = "auth/trackReviewMain";
+    public static final String ORDERS_PAGE = "admin/orders";
+    public static final String CREATE_ORDER_FORM = "user/orderForm";
+    public static final String ORDER_MAIN_PAGE = "admin/orderMain";
+    public static final String USERS_PAGE = "admin/users";
+    public static final String SET_OPTIONAL_INFO_PAGE = "user/setOptionalInfo";
+    public static final String UPDATE_BALANCE_PAGE = "user/updateBalanceForm";
+    public static final String USER_PROFILE_PAGE = "auth/userProfile";
+    public static final String LOGIN_PAGE = "login";
+    public static final String USER_INFO_PAGE = "user/userInfo";
+    public static final String REGISTRATION_PAGE = "register";
+    public static final String ADD_ADMIN_FORM = "admin/addAdminForm";
+    public static final String USER_WELCOME_PAGE = "user/welcome";
+    public static final String ALBUM_UPDATE_PAGE = "admin/albumUpdateForm";
+    public static final String USER_ORDERS = "user/userOrders";
+    public static final String MIX_TRACKS_PAGE = "auth/mixTracks";
+    public static final String ADMIN_USER_ORDERS_PAGE = "admin/userOrders";
+    public static final String CANCELLED_ORDERS_PAGE = "admin/cancelledOrders";
+    public static final String TRACK_ORDERS_PAGE = "admin/trackOrders";
+    public static final String USER_ALBUM_REVIEWS_PAGE = "auth/userAlbumReviews";
+    public static final String USER_MIX_REVIEWS_PAGE = "auth/userMixReviews";
+    public static final String USER_TRACK_REVIEWS_PAGE = "auth/userTrackReviews";
+    public static final String CONCRETE_TRACK_REVIEWS_PAGE = "auth/concreteTrackReviews";
+    public static final String UPDATE_DPL_PAGE = "admin/updateDpl";
+    public static final String CONCRETE_MIX_REVIEWS_PAGE = "auth/concreteMixReviews";
+}
